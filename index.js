2fc2b2cbb93f711a305e32fc93ed6f9ccbbe08a3
f4faysal <f4faysals@gmai.com.com>
2022-11-24 03:32:11 +0600

    add install mongodb add a vercel and install dot env

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-const app = express();
+// middlewares
+app.use(cors())
+app.use(express.json())
 
-// middleware
-app.use(cors());
-app.use(express.json());
+// Database Connection
+const uri = process.env.DB_URI
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+async function run() {
+  try {
+    const homesCollection = client.db('puranbazar').collection('category')
+
+    console.log('Database Connected...')
+  } finally {
+  }
+}
+
+run().catch(err => console.error(err))
 
 app.get("/", async (req, res) => {
-  res.send("Puran bazar server  is running .........");
+  res.send("Puran bazar server  is running ....");
 });
 
 app.listen(port, () => console.log(`Doctors portal running on ${port}`));

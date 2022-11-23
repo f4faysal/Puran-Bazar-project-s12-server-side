ff921b2dfe76bf49de0903c72c9f306ad0a23c5f
f4faysal <f4faysals@gmai.com.com>
2022-11-24 02:35:24 +0600

    surer is runniing

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -10,11 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
 app.get("/", async (req, res) => {
-    res.send("doctors portal server is running");
-  });
-  
+  res.send("Puran bazar server  is running .........");
+});
+
 app.listen(port, () => console.log(`Doctors portal running on ${port}`));

469282d885ce75140e01052de30b2af0d13d1fcf
f4faysal <f4faysals@gmai.com.com>
2022-11-25 23:19:06 +0600

    add acatagry api

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Database Connection
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    const homesCollection = client.db('puranbazar').collection('category')
+    /**=======================================
+                     mongodb start 
+      ======================================*/
+    const categoryCollection = client.db("puranbazar").collection("category");
+    /**=======================================
+                       mongodb End 
+      ======================================*/
+    //-------------------------------------------------------------------------------------
+    /**=======================================
+                get api categories
+      =======================================*/
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
 
-    console.log('Database Connected...')
+    app.get("/categories/:slug", async (req, res) => {
+      const slug = req.params.slug;
+      console.log(slug);
+      const query = { slug: slug };
+      const categorie = await categoryCollection.findOne(query);
+      res.send(categorie);
+    });
+    /**=======================================
+                  Post service
+      =======================================*/
   } finally {
   }
 }
 
-run().catch(err => console.error(err))
+run().catch((err) => console.error(err));
 
 app.get("/", async (req, res) => {
   res.send("Puran bazar server  is running ....");
